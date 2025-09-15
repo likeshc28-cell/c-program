d4a07d270af0e8199e5c2bee8126b9d1bac37ba4
likeshc28-cell <likeshc28@gmail.com>
2025-09-15 11:57:12 +0530

    Add files via upload

---
+++ Q3.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main (){
+	int n;
+	scanf ("%d",&n);
+	printf((n%2==0)?"EVEN":"odd");
+	return 0;
+}
---
+++ Q5.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+	int n;
+	scanf ("%d",&n);	
+if(n >=100 &&n <=999);
+	printf("%d",n);
+	return 0;
+}
+
+  
